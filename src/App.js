29059e52df1dfecb38d6a4458dc2ed93d18db370
denshou <132555762+denshou@users.noreply.github.com>
2023-09-10 12:28:43 +0900

    news 검색 구현
    topic입력되지 않으면 안됨
    날짜 입력 시 해당 날짜부터 지금까지 publish된 news를 보여줌
    무한 스크롤 구현

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Loading from "./components/Loading";
 import { useInView } from "react-intersection-observer";
 
 function App() {
   const [articles, setArticles] = useState([]);
-  const [topic, setTopic] = useState("");
-  const [from, setFrom] = useState(null);
+  const [topic, setTopic] = useState("tesla");
+  const [from, setFrom] = useState("2023-09-09");
   const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = useState(1);
+  const [ref, inView] = useInView();
 
   const [url, setUrl] = useState(
-    "https://newsapi.org/v2/top-headlines?country=kr&page=1&apiKey=94b00823b790482094d652f6cd3b55b2"
+    `https://newsapi.org/v2/everything?q=${topic}&from=${from}&pageSize=10&page=${page}&sortBy=publishedAt&apiKey=3ff5a28debea4f11892ef4dd34f978bf`
   );
 
-  useEffect(() => {
-    async function fetchData() {
-      setIsLoading(true);
-      try {
-        await fetch(url)
-          .then((res) => res.json())
-          .then((data) => setArticles(data.articles));
-        setIsLoading(false);
-      } catch (error) {
-        window.alert(error);
-      }
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      await fetch(url)
+        .then((res) => res.json())
+        .then((data) => setArticles([...articles, ...data.articles]));
+
+      setIsLoading(false);
+      console.log(articles);
+    } catch (error) {
+      window.alert(error);
     }
+  };
+
+  useEffect(() => {
     fetchData();
   }, [url]);
+
+  useEffect(() => {
+    if (inView) {
+      setPage((prevState) => prevState + 1);
+      // if (topic) {
+      setUrl(
+        `https://newsapi.org/v2/everything?q=${topic}&from=${from}&pageSize=10&page=${page}&sortBy=publishedAt&apiKey=3ff5a28debea4f11892ef4dd34f978bf`
+      );
+      // }
+
+      // if (topic === "") {
+      //   setUrl(
+      //     `https://newsapi.org/v2/everything?q=${topic}&from=${from}&pageSize=10&page=${page}&sortBy=publishedAt&apiKey=3ff5a28debea4f11892ef4dd34f978bf`
+      //   );
+      // }
+    }
+  }, [inView]);
 
   const inputChange = (event) => {
     setTopic(event.target.value);
@@ -37,16 +58,26 @@ function App() {
   };
   const submitHandler = (event) => {
     event.preventDefault();
+
     if (topic === "") {
       window.alert("검색어를 입력해주세요.");
       return;
     }
-    setUrl(
-      `https://newsapi.org/v2/everything?q=${topic}&from=${from}&to=${from}&page=${page}&sortBy=publishedAt&apiKey=5ff16da1c98c4f08aa5dcdb903ad4df1`
-    );
-  };
 
-  console.log(articles);
+    setArticles([]);
+    setPage(1);
+    if (topic) {
+      setUrl(
+        `https://newsapi.org/v2/everything?q=${topic}&from=${from}&pageSize=10&page=${page}&sortBy=publishedAt&apiKey=3ff5a28debea4f11892ef4dd34f978bf`
+      );
+    }
+
+    if (!topic) {
+      setUrl(
+        `https://newsapi.org/v2/everything?q=${topic}&from=${from}&pageSize=10&page=${page}&sortBy=publishedAt&apiKey=3ff5a28debea4f11892ef4dd34f978bf`
+      );
+    }
+  };
   return (
     <>
       {isLoading && <Loading />}
@@ -82,6 +113,7 @@ function App() {
               </div>
             </div>
           ))}
+          <div ref={ref}></div>
         </div>
       </div>
     </>
